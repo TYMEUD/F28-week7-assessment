3528defb45a0b3d899006cd6abd49334bca15db3
Ty Menlove <tjmundecided@gmail.com>
2022-09-30 14:42:53 -0700

    fixed add to zero function

--- code.js
+++ code.js
@@ -1,12 +1,16 @@
- const addToZero = (arr) => {
-for (let i = 0; i < arr.length; i++) {
-   for (let j = 0; j < arr.length; j--) {
-   if (arr[i] + arr[j] === 0)
-   return true 
-   else return false
-   } 
+const addToZero = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+        for(j = i + 1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return true
+            }
+        }
     }
- }
+
+    return false
+}
+
+
 
  console.log(addToZero([1,2,3]))
  console.log(addToZero([1,2,3,-3]))
