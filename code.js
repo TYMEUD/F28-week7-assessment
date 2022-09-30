154283f51aaa2b094398bbf539eba0d9dd30e8b7
Ty Menlove <tjmundecided@gmail.com>
2022-09-30 15:17:09 -0700

    added pangram and longest word function

--- code.js
+++ code.js
@@ -12,9 +12,9 @@ const addToZero = (arr) => {
 
 
 
- console.log(addToZero([1,2,3]))
- console.log(addToZero([1,2,3,-3]))
- console.log(addToZero([1,2,3,-1]))
+//  console.log(addToZero([1,2,3]))
+//  console.log(addToZero([1,2,3,-3]))
+//  console.log(addToZero([1,2,3,-1]))
 
 
  
@@ -34,8 +34,82 @@ const addToZero = (arr) => {
    return true
  }
  
- console.log(hasUniqueChars("Monday"))
- console.log(hasUniqueChars("Moody"))
- console.log(hasUniqueChars("oblong"))
+//  console.log(hasUniqueChars("Monday"))
+//  console.log(hasUniqueChars("Moody"))
+//  console.log(hasUniqueChars("oblong"))
 
   // Time O(n)
+
+  const isPangram = (str) => {
+    str = str.toLowerCase().split('')
+    
+    const alphabet = {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0, 
+        f: 0,
+        g: 0, 
+        h: 0, 
+        i: 0,
+        j: 0,
+        k: 0, 
+        l: 0,
+        m: 0,
+        n: 0,
+        o: 0,
+        p: 0,
+        q: 0,
+        r: 0,
+        s: 0,
+        t: 0,
+        u: 0,
+        v: 0,
+        w: 0,
+        x: 0,
+        y: 0,
+        z: 0
+    }
+
+    for(let i = 0; i < str.length; i++){
+        if(alphabet[str[i]] === 0){
+            alphabet[str[i]] += 1
+        }
+    }
+
+    for(let key in alphabet){
+        if(alphabet[key] === 0){
+            return false
+        }
+    }
+    
+    return true
+}
+
+ console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+// -> True
+
+console.log(isPangram("I like cats, but not mice"));
+// -> False
+
+// Time O(n)
+
+
+const findLongestWord = (arr) => {
+    let longestLength = 0
+
+    arr.forEach((word) => {
+        if(word.length > longestLength){
+            longestLength = word.length
+        }
+    })
+
+    return longestLength
+
+}
+
+console.log(findLongestWord(['bruh', 'canada','arasaka']))
+
+
+// Time O(n)
