533525f79bf8be6737e66dc31f05c5d25f79a32f
Ty Menlove <tjmundecided@gmail.com>
2022-09-30 14:11:24 -0700

    added functions

---
+++ code.js
@@ -0,0 +1,21 @@
+ const addToZero = (arr) => {
+for (let i = 0; i < arr.length; i++) {
+   for (let j = 0; j < arr.length; j--) {
+   if (arr[i] + arr[j] === 0)
+   return true 
+   else return false
+   } 
+    }
+ }
+
+ console.log(addToZero([1,2,3]))
+ console.log(addToZero([1,2,3,-3]))
+ console.log(addToZero([1,2,3,-1]))
+
+
+ const hasUniqueChars = (string) => {
+for (let i = 0; i < string.length; i++) {
+    
+    
+}
+ }
